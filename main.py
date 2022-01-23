953722602416b00443a257099d9653a8030f62de
Volkoff <antonmivo@gmail.com>
2022-01-23 03:10:42 +0100

    anotherone

--- main.py
+++ main.py
@@ -9,14 +9,49 @@ dictionary = {
     "phone" : "mobil"
 }
 
-def translateloc(str,conn):
+
+def translateloc(message, conn):
     try:
-        conn.send(f'TRANSLATESUC"{dictionary[str[1]]}"\r\n'.encode('utf-8'))
+        conn.send(f'TRANSLATESUC"{dictionary[message]}"\r\n'.encode('utf-8'))
     except KeyError:
         conn.send('TRANSLATEERR"not in this dictionary"\r\n'.encode("utf-8"))
-
-
 portsOpen = []
+ipport = []
+def checkipconnect():
+    #for i in range(256):
+        ip = "192.168.1.107"
+        print(ip)
+        for port in range(65531, 65533):
+            print(port)
+            try:
+                serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                serv.connect((ip, port))
+                serv.settimeout(0.01)
+                serv.recv(1024)
+                serv.send('check'.encode())
+                message = serv.recv(1024)
+                print(type(message))
+                if message.decode('utf-8') == 'hello':
+                    print("hey")
+                    ipport.append(ip + ":" + port)
+                print(ipport)
+            except Exception as e:
+                print(e)
+
+
+# def translaterem(conn):
+#     try:
+#         checkipconnect()
+#         serv.connect((ip, portsOpen[0]))
+#         serv.recv(1024)
+#         serv.send(f'TRANSLATELOC"{message[1]}"'.encode('utf-8'))
+#         data = serv.recv(1024)
+#         conn.send(data)
+#         print(portsOpen)
+#     except Exception as e:
+#         print(e)
+
+
 def clinet(conn):
     global running
     running = True
@@ -26,40 +61,24 @@ def clinet(conn):
         data = conn.recv(1024)
         if not data or data.decode('utf-8').lower() == 'end':
             break
+
         if data.decode('utf-8') == "\r\n" or data.decode('utf-8') == "\n":
             data = conn.recv(1024)
-        msgformethod = data.decode('utf-8')
-        message = data.decode('utf-8')
-        message = str(message)
-        message = message.split('"')
+        try:
+            message = data.decode('utf-8')
+            message = str(message)
+            message = message.split('"')
+            print(message[0])
+        except Exception as e:
+            print(e)
 
+        if message[0] == 'check':
+            conn.send("hello".encode())
         if message[0] == "TRANSLATELOC":
-            translateloc(msgformethod,conn)
+            translateloc(message[1],conn)
         elif message[0] == "TRANSLATEREM":
-            try:
-                for i in range(256):
-                    ip = "192.168.1.%d", i
-                    for port in range(65530, 65536):
-                        try:
-                            serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                            serv.connect((ip, port))
-                            socket.setdefaulttimeout(0.01)
-                            serv.recv(1024)
-                            serv.send(f'TRANSLATELOC"{message[1]}"'.encode('utf-8'))
-                            message = serv.recv(1024)
-                            conn.send(message)
-                            portsOpen.append(port)
-                        except Exception as e:
-                            print(e)
-                serv.connect((ip,portsOpen[0]))
-                serv.recv(1024)
-                serv.send(f'TRANSLATELOC"{message[1]}"'.encode('utf-8'))
-                data = serv.recv(1024)
-                conn.send(data)
-                conn.send(message)
-                print(portsOpen)
-            except:
-                print("OOF")
+            checkipconnect()
+            print(message[1])
         else:
             conn.send("Wrong".encode('utf-8'))
         print('received from client: %s' % data.decode("utf-8"))
